4622b5c7176d55e038bd98d60e226f047ef92105
Daniel Estrada <daniel.estrada.acevedo@cern.ch>
2024-02-01 02:49:15 -0500

    Change PID control for the PID arduino library

--- concept-test/firmware/oven_code/oven_code.ino
+++ concept-test/firmware/oven_code/oven_code.ino
@@ -1,77 +1,66 @@
 #include "thermocouple.h"
+#include <PID_v1.h>
 
 //code based on https://electronoobs.com/eng_arduino_tut24.php
 
-int PWM_pin = 9;
+int PWM_pin = 9; // PID output pin
+double Setpoint;   // temperature setpoint.
 
-float goal_temperature = 50;   //Default temperature setpoint.
-
-float temperature = 0;
-float PID_error = 0;
-float previous_error = 0;
-float elapsedTime, Time, prev_time;
-float PID_value = 0;
+double temperature = 0; // PID input
+double PID_output = 0; // PID output
 int button_pressed = 0;
 int menu_activated=0;
-float last_goal_temperature = 0;
 
-//PID constants
+//PID tunnnig parameters
 //////////////////////////////////////////////////////////
-int kp =20;   int ki = 40;   int kd = 0;
+double kp =2;  double ki = 5;  double kd = 1;
 //////////////////////////////////////////////////////////
 
-int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
+PID PID_controller(&temperature, &PID_output, &Setpoint, kp, ki, kd, DIRECT);
 
 void setup()
 {
     Serial.begin(9600);
     pinMode(PWM_pin, OUTPUT);
     set_thermocouple(4, 5, 6);
-    Serial.begin(9600);
-    Time = millis();
+    
+    // intialize variables
+    temperature = measure_temp();
+    Setpoint = 50;
+
+    // turn the PID on
+    PID_controller.SetMode(AUTOMATIC);
+    PID_controller.SetSampleTime(250);
 
 }
 
 void loop()
 {
-    // Read the real value of temperature
+    // Read the current value of temperature
     temperature = measure_temp();
-    //Serial.print("temperature = ");
+    PID_controller.Compute();
+
+    analogWrite(PWM_pin, 255-PID_output); // PID_output lives in [0, 255]
+
     Serial.println(temperature);
-//    Serial.print("\t PID = ");
-//    Serial.println(PID_value);
-    
-    //Next we calculate the error between the setpoint and the real value
-    PID_error = goal_temperature - temperature + 3;
 
-    //Calculate the P value
-    PID_p = 0.01 * kp * PID_error;
-
-    //Calculate the I value in a range on +-3
-    PID_i = 0.01 * PID_i + (ki * PID_error);
-    
-
-    //For derivative we need real time to calculate speed change rate
-    prev_time = Time;
-    Time = millis();
-    elapsedTime = (Time - prev_time) / 1000; 
-
-    //Calculate the D value
-    PID_d = 0.01 * kd * ((PID_error - previous_error) / elapsedTime);
-    //Final total PID value is the sum of P + I + D
-    PID_value = PID_p + PID_i + PID_d;
-    
-    PID_value = constrain(PID_value, 0, 255);
-
-    analogWrite(PWM_pin, 255-PID_value);
-    previous_error = PID_error;     //Store the error for next loop.
-
-    delay(250);
+    delay(250); // wait because the thermocouple resolution
 }
 
 void serialEvent(){
-    double value = Serial.parseFloat();
-    goal_temperature = constrain(value, 0, 200);
-    Serial.print("goal temp set to ");
-    Serial.println(goal_temperature);
+    String command  = Serial.readString();
+
+    if (command[0] == 'S'){
+        Setpoint = command.substring(1).toInt();
+    }
+    if (command[0] == 'k'){
+        if (command[1] == 'p')
+            kp = command.substring(2).toInt();
+        if (command[1] == 'i')
+            ki = command.substring(2).toInt();
+        if (command[1] == 'd')
+            kd = command.substring(2).toInt();
+
+        PID_controller.SetTunings(kd, ki, kd);
+    }
 }
