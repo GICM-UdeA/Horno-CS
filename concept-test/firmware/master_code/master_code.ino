d9c0e32a12a1e35cce741bf9173c4a3aea013b93
Daniel Estrada <daniel.estrada.acevedo@cern.ch>
2024-02-07 17:37:50 -0500

    Add PID control for the oven

--- concept-test/firmware/master_code/master_code.ino
+++ concept-test/firmware/master_code/master_code.ino
@@ -1,12 +1,26 @@
 /* I2C master code...*/
+/* This is the code of the main Arduino
+  It control the motor and the oven under
+  user serial instructions... 
+*/
 
-#include "max6675.h" // amplifier module for the Thermocouple
+#include "max6675.h" 
+#include <PID_v1.h>
 #include <Wire.h> // for I2C comunication
 
-// Thermocouple pins
-const int MAX6675_SO = 4;
-const int MAX6675_CS = 5;
-const int MAX6675_SCK = 6;
+// PID Thermocouple pins
+const int MAX6675_SO_PID = 4;
+const int MAX6675_CS_PID = 5;
+const int MAX6675_SCK_PID = 6;
+
+// monitoring Thermocouple pins
+const int MAX6675_SO = 7;
+const int MAX6675_CS = 8;
+const int MAX6675_SCK = 9;
+
+// setting up thermocouples
+MAX6675 PID_input_sensor(MAX6675_SCK_PID, MAX6675_CS_PID, MAX6675_SO_PID);
+MAX6675 temp_sensor(MAX6675_SCK, MAX6675_CS, MAX6675_SO);
 
 // to enable the measurement
 volatile bool measure_temp = false;
@@ -17,21 +31,49 @@ const int end_pin = 3;
 // speed variable
 int rpm_value = 0;
 
+int PWM_pin = 11; // PID output pin
+double Setpoint;   // temperature setpoint.
+
+double temperature = 0; // PID input
+double PID_output = 0; // PID output
+
+//PID tunnnig parameters
+//////////////////////////////////////////////////////////
+double kp =2;  double ki = 5;  double kd = 1;
+//////////////////////////////////////////////////////////
+
+PID PID_controller(&temperature, &PID_output, &Setpoint, kp, ki, kd, DIRECT);
+
 void setup() {
-  // setting up thermocouple pins
-  pinMode(MAX6675_CS, OUTPUT);
-  pinMode(MAX6675_SO, INPUT);
-  pinMode(MAX6675_SCK, OUTPUT);
-  pinMode(3, INPUT);
+
+  pinMode(end_pin, INPUT);
   Serial.begin(115200);
   Wire.begin();
+
+  // intialize variables
+  temperature = PID_input_sensor.readCelsius();
+  Setpoint = 50;
+
+  // turn the PID on
+  PID_controller.SetMode(AUTOMATIC);
+  PID_controller.SetSampleTime(250);
 
   attachInterrupt(digitalPinToInterrupt(end_pin), end_measure, FALLING);
 }
 
 void loop() {
   if (measure_temp){
-    Serial.println(readThermocouple());
+    temperature = PID_input_sensor.readCelsius();
+    PID_controller.Compute();
+
+    analogWrite(PWM_pin, 255-PID_output); // PID_output lives in [0, 255]
+
+    Serial.print("T_PID:");
+    Serial.print(temperature);
+    Serial.print(",");
+    Serial.print("T:");
+    Serial.println(temp_sensor.readCelsius());
+
     delay(250);
   }
 }
@@ -41,7 +83,7 @@ void serialEvent() {
   
   String command = Serial.readString();
   
-  if (command[0] == 'S'){ // S --> to set the speed (in rpm)
+  if (command[0] == 'V'){ // S --> to set the speed (in rpm)
     rpm_value = command.substring(1).toInt();
     set_rpm();
   } 
@@ -54,6 +96,19 @@ void serialEvent() {
   if (command[0] == 'i'){ // i --> to invert the movement direction
     invert_motor();
   }
+  if (command[0] == 's'){ // s --> to stablish the setPoint temperature
+        Setpoint = command.substring(1).toInt();
+    }
+    if (command[0] == 'k'){ // k+{p, i, d} --> to stablish the PID constants
+        if (command[1] == 'p')
+            kp = command.substring(2).toInt();
+        if (command[1] == 'i')
+            ki = command.substring(2).toInt();
+        if (command[1] == 'd')
+            kd = command.substring(2).toInt();
+
+        PID_controller.SetTunings(kd, ki, kd);
+    }
 }
 
 void set_rpm() {
@@ -103,35 +158,4 @@ void invert_motor(){
 void end_measure(){
   measure_temp = false;
   Serial.println("FIN");
-}
-
-double readThermocouple() {
-  // code taken from https://electronoobs.com/eng_arduino_tut24.php
-  uint16_t v;
-  
-  digitalWrite(MAX6675_CS, LOW);
-  delay(1);
-
-  // Read in 16 bits,
-  //  15    = 0 always
-  //  14..2 = 0.25 degree counts MSB First
-  //  2     = 1 if thermocouple is open circuit  
-  //  1..0  = uninteresting status
-  
-  v = shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
-  v <<= 8;
-  v |= shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
-  
-  digitalWrite(MAX6675_CS, HIGH);
-  if (v & 0x4) 
-  {    
-    // Bit 2 indicates if the thermocouple is disconnected
-    return NAN;     
-  }
-
-  // The lower three bits (0,1,2) are discarded status bits
-  v >>= 3;
-
-  // The remaining bits are the number of 0.25 degree (C) counts
-  return v*0.25;
 }
