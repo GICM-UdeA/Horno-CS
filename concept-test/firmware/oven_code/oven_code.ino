d66c4080d964cb9c1351aac0459df7ed719ce8bd
Daniel Estrada <daniel.estrada.acevedo@cern.ch>
2024-02-07 17:36:53 -0500

    Add an extra thermocuple to monitor oven temperature

--- concept-test/firmware/oven_code/oven_code.ino
+++ concept-test/firmware/oven_code/oven_code.ino
@@ -1,9 +1,9 @@
-#include "thermocouple.h"
+#include "max6675.h"
 #include <PID_v1.h>
 
 //code based on https://electronoobs.com/eng_arduino_tut24.php
 
-int PWM_pin = 9; // PID output pin
+int PWM_pin = 11; // PID output pin
 double Setpoint;   // temperature setpoint.
 
 double temperature = 0; // PID input
@@ -17,15 +17,17 @@ double kp =2;  double ki = 5;  double kd = 1;
 //////////////////////////////////////////////////////////
 
 PID PID_controller(&temperature, &PID_output, &Setpoint, kp, ki, kd, DIRECT);
+MAX6675 temp_sensor(6, 5, 4);
+MAX6675 temp_sensor_check(9, 8, 7);
+
 
 void setup()
 {
     Serial.begin(9600);
     pinMode(PWM_pin, OUTPUT);
-    set_thermocouple(4, 5, 6);
-    
+
     // intialize variables
-    temperature = measure_temp();
+    temperature = temp_sensor.readCelsius();
     Setpoint = 50;
 
     // turn the PID on
@@ -37,12 +39,16 @@ void setup()
 void loop()
 {
     // Read the current value of temperature
-    temperature = measure_temp();
+    temperature = temp_sensor.readCelsius();
     PID_controller.Compute();
 
     analogWrite(PWM_pin, 255-PID_output); // PID_output lives in [0, 255]
 
-    Serial.println(temperature);
+    Serial.print("T_PID:");
+    Serial.print(temperature);
+    Serial.print(",");
+    Serial.print("T:");
+    Serial.println(temp_sensor_check.readCelsius());
 
     delay(250); // wait because the thermocouple resolution
 }
